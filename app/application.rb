feec90fcb8fc449ad05c6e3adaca8d35a4e93c16
David Charles <ratlover03@gmail.com>
2019-06-29 00:48:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.new
-
+    if time <= 12
+      resp.write "good afternoon"
+    else
+      resp.write "good morning"
   end
 
 end
